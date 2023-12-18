09e9f0268046b075730658a6ce911746320edb19
heldinho <hfpsis25@gmail.com>
2023-12-17 23:16:12 -0300

    fix

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const url = 'https://randomuser.me/api/'
 async function getRandomUserData() {
   try {
     const response = await axios.get(url)
-
-    console.log(response.data)
-
     const userData = response.data.results[0]
-
+    const user = {
+      nome: `${userData.name.first} ${userData.name.last}`,
+      email: userData.email,
+      pais: userData.location.country,
+    }
     console.log('Nome: ', userData.name.first, userData.name.last)
     console.log('Email: ', userData.email)
     console.log('País: ', userData.location.country)
-    // Adicione mais informações conforme necessário
+    console.log(user)
   } catch (error) {
     console.error('Erro ao obter dados do usuário aleatório:', error.message)
   }
